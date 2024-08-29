37cb95ae35cf59b328416c2558b6a183fae6985e
Daniel <danielholyoak9@gmail.com>
2024-08-29 15:45:54 +0200

    polls/models.py

--- main_proj/polls/models.py
+++ main_proj/polls/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 
+"""
+Create your models here.
+This Python class defines a model for a question with text and
+publication date fields.
+"""
 
-# Create your models here.
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date_published0')
 
     def __str__(self):
         return self.question_text
+
+
+"""
+The `Choice` class represents a choice option for a question with attributes for choice text and
+number of votes.
+"""
 
 
 class Choice(models.Model):
