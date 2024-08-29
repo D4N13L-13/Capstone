05e4e9a4366923573b483773ad6bf217f6aaaa32
Daniel <danielholyoak9@gmail.com>
2024-08-29 15:48:45 +0200

    polls/admin.py

--- main_proj/polls/admin.py
+++ main_proj/polls/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Question, Choice
 
 # Register your models here.
+# Registering the `Question` and `Choice`
+# Managed and viewed through the Django admin site.
+# Once registered, you can access and interact with instances of these
+# models through the admin interface
 admin.site.register(Question)
 admin.site.register(Choice)
