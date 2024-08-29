baffe32da4ce8a9d9a9e967803e4bac17a84c1dd
Daniel <danielholyoak9@gmail.com>
2024-08-29 15:05:27 +0200

    Added documentation for main_proj/urls.py

--- main_proj/main_proj/urls.py
+++ main_proj/main_proj/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# Configuring the URL patterns for  project.
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('polls.urls')),
