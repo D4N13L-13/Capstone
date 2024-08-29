a6b3bb557c6cd5d1a3d4dc5d212dc700672dffbc
Daniel <danielholyoak9@gmail.com>
2024-08-29 14:51:28 +0200

    Added documentation for auth_user/forms

--- main_proj/auth_user/forms.py
+++ main_proj/auth_user/forms.py
@@ -3,10 +3,22 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 
+"""
+The `RegisterForm` class is a form for user registration that includes
+fields for username, email, and password.
+"""
+
+
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+"""
+The `LoginForm` class extends `AuthenticationForm` and defines fields
+for username and password with placeholder attributes.
+"""
 
 
 class LoginForm(AuthenticationForm):
