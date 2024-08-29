8da93f5ced0837af3ac98ca28be6968d6a5702f5
Daniel <danielholyoak9@gmail.com>
2024-08-29 15:37:32 +0200

    main_proj/urls.py

--- main_proj/main_proj/urls.py
+++ main_proj/main_proj/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 # Configuring the URL patterns for  project.
+# Routing incoming URLs to the appropriate views in project.
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('polls.urls')),
