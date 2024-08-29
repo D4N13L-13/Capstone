62d39f6203a08e63f747f0d6ea4b06eb9a33f58e
Daniel <danielholyoak9@gmail.com>
2024-08-29 15:14:01 +0200

    Added documentation for main_proj/apps.py

--- main_proj/myCV/apps.py
+++ main_proj/myCV/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
+# Defines configuration settings for the `myCV`.
 class MycvConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myCV'
