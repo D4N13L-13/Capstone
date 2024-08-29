2a64e5c6538eaa71885bb87d9c6aea34ee8a53db
Daniel <danielholyoak9@gmail.com>
2024-08-29 14:48:18 +0200

    Added comments for auth_user/urls

--- main_proj/auth_user/urls.py
+++ main_proj/auth_user/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+# Defining URL patterns for web application.
+# Each Path function call, specifies a URL pattern along with the
+# corresponding view function that should be called
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('register/', views.register, name='register'),
